adf1d195e3c3d3fd0eba4f23936a667dfa85fe11
Adarsh J <adarsh895j@gmail.com>
2021-08-03 17:34:20 +0530

    Window Created

---
+++ Calulator_Python.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+window.title("Simple Calculator")
+# Set window size
+window.geometry("463x500")
+# Set window background color
+window.configure(bg="#999999")
+
+
+window.mainloop()
